c8401630913e66b395faf9a7ff8577f84fcef508
esthoril <shendrickx@hotmail.com>
2018-01-09 17:34:40 +0100

    update folder structure

---
+++ arduino-nbiot-sdk/examples/cbor/cbor.ino
@@ -0,0 +1,80 @@
+/*    _   _ _ _____ _    _              _____     _ _     ___ ___  _  __
+ *   /_\ | | |_   _| |_ (_)_ _  __ _ __|_   _|_ _| | |__ / __|   \| |/ /
+ *  / _ \| | | | | | ' \| | ' \/ _` (_-< | |/ _` | | / / \__ \ |) | ' <
+ * /_/ \_\_|_| |_| |_||_|_|_||_\__, /__/ |_|\__,_|_|_\_\ |___/___/|_|\_\
+ *                             |___/
+ *
+ * Copyright 2018 AllThingsTalk
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include <PayloadBuilder.h>
+#include <ArduinoCbor.h>
+#include "ATT_NBIOT.h"
+
+// Mbili support
+#define DEBUG_STREAM Serial
+#define MODEM_STREAM Serial1
+#define MODEM_ON_OFF_PIN 23
+
+#define baud 9600
+
+// AllThingsTalk Device
+const char* deviceid = "Ddjdc1aYskfXGva9z6gelQWO";
+const char* devicetoken = "spicy:4O3SMDqz3ygu80Nw7ybfJYdrR1FwCzN5fMFwuTD1";
+
+ATT_NBIOT nbiot;
+PayloadBuilder payload(nbiot);
+
+void setup()
+{
+  delay(3000);
+
+  DEBUG_STREAM.begin(baud);
+  MODEM_STREAM.begin(baud);
+  
+  DEBUG_STREAM.println("Initializing and connecting... ");
+
+  nbiot.setAttDevice(deviceid, devicetoken);
+  nbiot.init(MODEM_STREAM, DEBUG_STREAM, MODEM_ON_OFF_PIN);
+  
+  if(nbiot.connect())
+    DEBUG_STREAM.println("Connected!");
+  else
+  {
+    DEBUG_STREAM.println("Connection failed!");
+    while(true) {}  // No connection. No need to continue the program
+  }
+  
+  CborBuffer buffer(200);
+  CborObject object = CborObject(buffer);
+  
+  object.set("string", "helloworld");
+  object.set("integer", -1234);
+}
+
+int counter = 1;  // Initialize counter
+unsigned long sendNextAt = 0;  // Keep track of time
+void loop() 
+{
+  /*
+  if(sendNextAt < millis())
+  {
+    nbiot.sendMessage(counter, "counter");  // Send counter value
+   
+    counter++;
+    sendNextAt = millis() + 100000;
+  }
+  */
+}
